5cc93729d129eeabd76e78a3a4ad7233e2a09221
cherkasovaa <alina.ch1202@gmail.com>
2025-07-10 15:53:13 +0300

    feat: implement Search Bar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,14 @@
 import { Container } from '@/components/Container';
+import { SearchBar } from '@/components/SearchBar';
 import { Component } from 'react';
 
 export class App extends Component {
   render() {
     return (
-      <main className="min-h-screen bg-neutral-800 text-gray-100">
-        <Container>App component</Container>
+      <main className="min-h-screen bg-neutral-800 text-gray-100 py-6">
+        <Container>
+          <SearchBar />
+        </Container>
       </main>
     );
   }
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,59 @@
+import { Button } from '@/components/Button';
+import { SearchInputField } from '@/components/SearchInputField';
+import { storage } from '@/services/Storage';
+import type { SearchBarProps, SearchInputState } from '@/types/interfaces';
+import { Component, type ChangeEvent, type FormEvent } from 'react';
+
+export class SearchBar extends Component<SearchBarProps, SearchInputState> {
+  constructor(props: SearchBarProps) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(): void {
+    const savedSearchTerm = storage.getSearchTerm();
+
+    if (savedSearchTerm) {
+      this.setState({ value: savedSearchTerm });
+
+      if (this.props.onSearch) {
+        this.props.onSearch(savedSearchTerm);
+      }
+    }
+  }
+
+  handleChange(event: ChangeEvent<HTMLInputElement>): void {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+
+    const searchTerm = this.state.value.trim();
+    storage.setSearchTerm(searchTerm);
+
+    if (this.props.onSearch) {
+      this.props.onSearch(searchTerm);
+    }
+
+    this.setState({ value: '' });
+  }
+
+  render() {
+    return (
+      <form
+        className="py-2 grid grid-cols-1 place-items-end sm:grid-cols-[1fr_auto] gap-6"
+        onSubmit={this.handleSubmit}
+      >
+        <SearchInputField
+          onChange={this.handleChange}
+          value={this.state.value}
+        />
+        <Button content="Search" type="submit" />
+      </form>
+    );
+  }
+}
