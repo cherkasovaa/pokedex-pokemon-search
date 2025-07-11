ce27a89b75dec211172b94108e7444ce33dca94f
cherkasovaa <alina.ch1202@gmail.com>
2025-07-11 10:11:43 +0300

    feat: render detailed or simple card based on data type

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,13 +1,22 @@
+import { DetailedCard } from '@/components/DetailedCard';
+import { SimpleCard } from '@/components/SimpleCard';
 import type { CardListProps } from '@/types/interfaces';
+import { isDetailedPokemon } from '@/types/typeGuards';
 import { Component } from 'react';
 
 export class CardList extends Component<CardListProps> {
   render() {
-    // const { results } = this.props;
+    const { results } = this.props;
 
     return (
-      <div className="h-full bg-neutral-700 rounded-lg p-4 overflow-auto">
-        CardList
+      <div className="flex flex-wrap justify-center  p-4 overflow-auto">
+        {results.map((pokemon) =>
+          isDetailedPokemon(pokemon) ? (
+            <DetailedCard key={pokemon.id} pokemon={pokemon} />
+          ) : (
+            <SimpleCard key={pokemon.name} pokemon={pokemon} />
+          )
+        )}
       </div>
     );
   }
