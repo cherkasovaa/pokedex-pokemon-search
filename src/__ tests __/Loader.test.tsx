31090dedaa211dc776863fc8d5f64e0f85ab77c6
cherkasovaa <alina.ch1202@gmail.com>
2025-07-18 18:29:43 +0300

    test: add tests for Loader class

---
+++ src/__ tests __/Loader.test.tsx
@@ -0,0 +1,13 @@
+import { Loader } from '@/components/Loader';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+
+describe('Loader component', () => {
+  test('renders spinner', () => {
+    render(<Loader />);
+
+    const spinner = screen.getByText('', { selector: 'div.animate-spin' });
+
+    expect(spinner).toBeInTheDocument();
+  });
+});
