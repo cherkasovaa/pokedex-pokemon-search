ba14a8c08843eed4035d4b80e0b9eeecfa280632
cherkasovaa <alina.ch1202@gmail.com>
2025-07-27 15:22:00 +0300

    feat: update Footer component
    - move the creator's name, the link to the portfolio's website, and the year of creation to a separate file

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,21 @@
+import {
+  CREATION_YEAR,
+  CREATOR_NAME,
+  PORTFOLIO_WEBSITE_LINK,
+} from '@/components/Footer/constants';
+
 export const Footer = () => {
   return (
     <footer className="text-center py-4 border-t-1 border-t-gray-500/30">
-      © 2025{' '}
+      © {CREATION_YEAR}{' '}
       <a
-        href="https://cherkasovaa.vercel.app/"
+        href={PORTFOLIO_WEBSITE_LINK}
         target="_blank"
         rel="noopener noreferrer"
         className="text-red-800 hover:text-red-800/80 duration-300"
         aria-label={"Link to the owner's portfolio website"}
       >
-        Alina Cherkasova
+        {CREATOR_NAME}
       </a>
     </footer>
   );
---
+++ src/components/Footer/constants.ts
@@ -0,0 +1,3 @@
+export const CREATION_YEAR = '2025';
+export const CREATOR_NAME = 'Alina Cherkasova';
+export const PORTFOLIO_WEBSITE_LINK = 'https://cherkasovaa.vercel.app/';
