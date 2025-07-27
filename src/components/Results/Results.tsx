cee01d7893302b5d77034b424ee30e20437117f6
cherkasovaa <alina.ch1202@gmail.com>
2025-07-27 19:02:19 +0300

    fix: correct Results render logic for empty data

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -11,12 +11,15 @@ export const Results = () => {
       <div className="flex-grow">
         {isLoading && <Loader />}
         {error && <ErrorMessage message={error} />}
-        {!isLoading && !error && results?.length ? (
-          <CardList results={results} />
-        ) : (
+
+        {!isLoading && !error && results?.length === 0 && (
           <p className="text-2xl text-gray-300 text-center">
             There is no data to display. Try again
           </p>
+        )}
+
+        {!isLoading && !error && results?.length && (
+          <CardList results={results} />
         )}
       </div>
     </div>
