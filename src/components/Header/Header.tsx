1f5d0144536036be41567a3f360bebd03dc0c56c
cherkasovaa <alina.ch1202@gmail.com>
2025-08-14 21:04:00 +0300

    feat: implement lang toggle button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,28 @@
 'use client';
 
 import { Container, ThemeSwitcher } from '@/components';
-import { Link } from '@/i18n/navigation';
+import { Link, usePathname, useRouter } from '@/i18n/navigation';
 import { APP_ROUTES } from '@/router/routes';
-import { useTranslations } from 'next-intl';
-import { usePathname } from 'next/navigation';
+import { useLocale, useTranslations } from 'next-intl';
 
 export const Header = () => {
   const pathname = usePathname();
+  const router = useRouter();
+  const locale = useLocale();
+
   const t = useTranslations('Navigation');
+
+  const toggleLanguage = () => {
+    router.push(pathname, { locale: locale === 'en' ? 'ru' : 'en' });
+  };
 
   return (
     <header
       role="banner"
       className="text-center py-4 border-b-1 border-b-foreground-muted/30"
     >
-      <Container className="flex justify-between py-0">
-        <nav>
+      <Container className="flex items-center justify-end gap-4 py-0">
+        <nav className="mr-auto">
           {APP_ROUTES.map((route) => {
             if (route.meta.isShowInNavigation) {
               const isActive = pathname === route.path;
@@ -36,6 +42,14 @@ export const Header = () => {
           })}
         </nav>
 
+        <div
+          className="bg-foreground/10 rounded-full px-6 py-1 cursor-pointer"
+          onClick={toggleLanguage}
+        >
+          <button className="text-accent text-sm transition-colors">
+            {locale.toUpperCase()}
+          </button>
+        </div>
         <ThemeSwitcher />
       </Container>
     </header>
